6da99cc4f61637a4199b4cae2df8d5e82d81c233
Justin Keung <justinkeung1018@gmail.com>
2024-11-03 15:38:49 +0000

    Move summary display below

--- src/components/BonusDisplay/index.tsx
+++ src/components/BonusDisplay/index.tsx
@@ -16,34 +16,38 @@ type BonusDisplayProps = {
 }
 
 export default function BonusDisplay({ parts, directs, tournament, questionSet, navOptions, bonusSummary }: BonusDisplayProps) {   
-    return <div className="flex flex-col md:flex-row md:space-x-10">
-        <div className="md:basis-1/2">
-            <h3 className="text-xl font-bold my-3">Question</h3>
-            {!!navOptions && !!tournament && <div className="mb-2">
-                {!!navOptions.previous && <Link href={`/tournament/${tournament.slug}/bonus/${navOptions.previous.round}/${navOptions.previous.number}`} className="underline">Previous bonus</Link>}
-                {!!navOptions.previous && !!navOptions.next && " - "}
-                {!!navOptions.next && <Link href={`/tournament/${tournament.slug}/bonus/${navOptions.next.round}/${navOptions.next.number}`} className="underline">Next bonus</Link>}
-            </div>}
-            <BonusText parts={parts} />
-            {!!parts[0]?.metadata && <div>{"<" + parts[0]?.metadata + ">"}</div>}
-            {(!!tournament || !!questionSet) && <p className="mt-2">
-                <Link 
-                    href={tournament ? `/tournament/${tournament.slug}/bonus` : `/set/${questionSet!.slug}/bonus`} 
-                    className="underline"
-                >
-                    Back to bonuses
-                </Link>
-            </p>}
-        </div>
-        <div className="md:basis-1/2">
-            <h3 className="text-xl font-bold my-3">Data</h3>
-            <div className="buzzes">
-                <BonusData directs={directs} />
+    return <>
+        <div className="flex flex-col md:flex-row md:space-x-10">
+            <div className="md:basis-1/2">
+                <h3 className="text-xl font-bold my-3">Question</h3>
+                {!!navOptions && !!tournament && <div className="mb-2">
+                    {!!navOptions.previous && <Link href={`/tournament/${tournament.slug}/bonus/${navOptions.previous.round}/${navOptions.previous.number}`} className="underline">Previous bonus</Link>}
+                    {!!navOptions.previous && !!navOptions.next && " - "}
+                    {!!navOptions.next && <Link href={`/tournament/${tournament.slug}/bonus/${navOptions.next.round}/${navOptions.next.number}`} className="underline">Next bonus</Link>}
+                </div>}
+                <BonusText parts={parts} />
+                {!!parts[0]?.metadata && <div>{"<" + parts[0]?.metadata + ">"}</div>}
+                {(!!tournament || !!questionSet) && <p className="mt-2">
+                    <Link 
+                        href={tournament ? `/tournament/${tournament.slug}/bonus` : `/set/${questionSet!.slug}/bonus`} 
+                        className="underline"
+                    >
+                        Back to bonuses
+                    </Link>
+                </p>}
             </div>
-            <h3 className="text-xl font-bold my-3">Summary</h3>
-                <div>
-                    <BonusSummaryDisplay bonusSummary={bonusSummary} tournament={tournament} />
+            <div className="md:basis-1/2">
+                <h3 className="text-xl font-bold my-3">Data</h3>
+                <div className="buzzes">
+                    <BonusData directs={directs} />
                 </div>
+            </div>
         </div>
-    </div>
+        <div className="flex justify-center">
+            <div>
+                <h3 className="text-xl font-bold my-3">Summary</h3>
+                <BonusSummaryDisplay bonusSummary={bonusSummary} tournament={tournament} />
+            </div>
+        </div>
+    </>
 }
